865437f6206d7c89ee738152fcb4f38137e6747e
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-11-22 13:21:59 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a17cb7b09f3c079bc9b3";
+/******/ 	var hotCurrentHash = "9b250a472c34eed302aa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/qgHxcsVvSVYBg9sCy1Fwf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fccc6f86fed3f7cbeb0f2.css","static\u002Fchunks\u002Fpages\u002Findex-6ade3c691db4aafd9611.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ec27b60e1d2da42fed28.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
