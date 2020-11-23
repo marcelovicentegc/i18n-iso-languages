310b1a3fb489e77eecccd2ff8a62f53e13b638d4
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-11-23 20:02:59 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b250a472c34eed302aa";
+/******/ 	var hotCurrentHash = "961fcb59e1fbb2be76b3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
